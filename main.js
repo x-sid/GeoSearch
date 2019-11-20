3b210ca0fdfd2fe88692f722c65c092564bb853f
x-sid <sidneyihunwaeze@gmail.com>
2019-11-20 15:58:55 +0100

    package.json file

--- main.js
+++ main.js
@@ -81,56 +81,54 @@ addressForm.addEventListener("submit", e => {
 function weatherInfo(lat, lon) {
   let proxy = "https://cors-anywhere.herokuapp.com/";
   let api = `${proxy}https://api.darksky.net/forecast/e3968dcc1c744f252da0bd34d9ae19e9/${lat},${lon}`;
-  try {
-    axios
-      .get(api, {
-        params: { crossDomain: true }
-      })
+  axios
+    .get(api, {
+      params: { crossDomain: true }
+    })
 
-      .then(res => {
-        //returned weather properties from the call to the api
-        let {
-          temperature,
-          summary,
-          icon,
-          humidity,
-          windSpeed
-        } = res.data.currently;
-        let timezone = res.data.timezone;
-        setIcon(icon, document.querySelector("#icon"));
-        let celcius = ((temperature - 32) * 5) / 9;
-        let currentTemp = document.querySelector(".degrees");
-        currentTemp.innerHTML = `${temperature}F <p id="tempConvert">click to convert</p>`;
-        //displays the required properties to the appropriate HTML DOM
-        document.querySelector(
-          "#timezone"
-        ).textContent = `Timezone ${timezone}`;
-        document.querySelector(
-          "#visibility"
-        ).innerHTML = `<h4 class="props"><strong>Humidity:</strong> ${humidity}</h4>`;
-        document.querySelector(
-          "#windspeed"
-        ).innerHTML = `<h4 class="props"><strong>Windspeed:</strong> ${windSpeed}</h4>`;
-        document.querySelector(
-          "#desc"
-        ).innerHTML = `<h4 class="props"><i>The current weather is ${summary}</i></h4>`;
+    .then(res => {
+      //returned weather properties from the call to the api
+      let {
+        temperature,
+        summary,
+        icon,
+        humidity,
+        windSpeed
+      } = res.data.currently;
+      let timezone = res.data.timezone;
+      setIcon(icon, document.querySelector("#icon"));
+      let celcius = ((temperature - 32) * 5) / 9;
+      let currentTemp = document.querySelector(".degrees");
+      currentTemp.innerHTML = `${temperature}F <p id="tempConvert">click to convert</p>`;
+      //displays the required properties to the appropriate HTML DOM
+      document.querySelector("#timezone").textContent = `Timezone ${timezone}`;
+      document.querySelector(
+        "#visibility"
+      ).innerHTML = `<h4 class="props"><strong>Humidity:</strong> ${humidity}</h4>`;
+      document.querySelector(
+        "#windspeed"
+      ).innerHTML = `<h4 class="props"><strong>Windspeed:</strong> ${windSpeed}</h4>`;
+      document.querySelector(
+        "#desc"
+      ).innerHTML = `<h4 class="props"><i>The current weather is ${summary}</i></h4>`;
 
-        //converts temperature between Celcius and Fahrenheit
-        currentTemp.addEventListener("click", () => {
-          if (currentTemp.innerHTML.indexOf("F") >= 0) {
-            currentTemp.textContent = `${Math.floor(celcius)}C`;
-          } else {
-            currentTemp.textContent = `${temperature}F`;
-          }
-        });
+      //converts temperature between Celcius and Fahrenheit
+      currentTemp.addEventListener("click", () => {
+        if (currentTemp.innerHTML.indexOf("F") >= 0) {
+          currentTemp.textContent = `${Math.floor(celcius)}C`;
+        } else {
+          currentTemp.textContent = `${temperature}F`;
+        }
       });
-  } catch (error) {
-    console.log(error.message);
-  }
+    })
+    .catch(error => {
+      console.log(error.message);
+    });
 }
 
 //functtion that sets the weather icon
 function setIcon(icon, iconId) {
+  //sets weather icon color
   const skycons = new Skycons({ color: "white" });
   const currentIcon = icon.replace(/-/g, "_").toUpperCase();
   skycons.play();
@@ -148,7 +146,7 @@ function displayMap(lat, lon) {
   // Initialize the map
   var map = L.map("map", {
     center: { lat: lat, lng: lon }, // Map loads with this location as center
-    zoom: 9,
+    zoom: 10,
     scrollWheelZoom: true,
     layers: [streets] // Show 'streets' by default
   });
